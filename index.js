94bf83c53a7f8c97baf9ca99700daf168ebd3976
JocelynGV <jv06042@georgiasouthern.edu>
2024-10-31 23:36:41 -0400

    More changes (still has bugs Im working on)

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 import express from "express";
+import bodyParser from "body-parser";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+//import bcrypt from bcrypt;
 // import anime from 'animejs'
+// const routes = require('src/routes/routes');
+// const bodyParser = require('body-parser');
 const _dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
 // const anime = require('animejs');
+
+// include middleware to process forms (gives the request a body)
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static(_dirname + '/public'));
 // app.use('/node_modules', express.static(_dirname + '/node_modules'));
@@ -19,6 +26,15 @@ app.get("/homepage", (req, res) => {
 app.get("/login", (req, res) => {
     console.log(_dirname + "/public/login.html");
     res.sendFile(_dirname + "/public/login.html");
+});
+
+app.get("/flashcards", (req, res) => {
+    console.log(_dirname + "/public/flashcards.html");
+    res.sendFile(_dirname + "/public/flashcards.html");
+});
+
+app.post("/submit", (req, res) => {
+    console.log(req.body);
 });
 
 app.listen(port, () => {
